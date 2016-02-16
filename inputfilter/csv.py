741ab7e7a07c0a2b7ab6c1c8f2715c8c54611966
arian <git@semioptimal.net>
2016-02-16 18:42:39 +0100

    csv inputfilter now returns numpy.array, disregards empty lines, comments

--- inputfilter/csv.py
+++ inputfilter/csv.py
@@ -1,9 +1,13 @@
 import numpy,csv
 
 def csv(infile,delimiter=','):
+    '''reads csv with arbitrary delimiter, returns numpy array of strings'''
     with open(infile) as f:
-        rv = [ l.strip().split(delimiter) for l in f ]
-        width = max(map(len,rv))
+        rv = [ l.strip().split(delimiter) for l in f
+                if l.strip()                            # no empty lines
+                and not l.startswith('#')               # no comments
+            ]
+        width = max(map(len,rv))                        # make array rectangular
         for l in rv:
             for i in range(len(l),width):
                 l.append('')
