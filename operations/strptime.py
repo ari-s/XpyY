773bb2bad976361b32f1cf85d4d8f9c0f7c1290b
arian <git@semioptimal.net>
2016-02-20 18:18:08 +0100

    strptime: add mul optional factor, e.g. for s->t conversion

--- operations/strptime.py
+++ operations/strptime.py
@@ -1,9 +1,9 @@
 import time,numpy,datetime,functools
 
 dateStr = '%Y-%m-%d-%H:%M'
-def strptime(data,dateStr=dateStr):
+def strptime(data,dateStr=dateStr,mul=1):
     first = datetime.datetime.strptime(data[0],dateStr)
-    return numpy.array([ (datetime.datetime.strptime(i,dateStr) - first).total_seconds() for i in data ],dtype=float)
+    return numpy.array([ (datetime.datetime.strptime(i,dateStr) - first).total_seconds() for i in data ],dtype=float)*mul
 
-def strptime2(dateStr):
-    return functools.partial(strptime,dateStr=dateStr)
+def strptime2(dateStr=dateStr,mul=1):
+    return functools.partial(strptime,dateStr=dateStr,mul=mul)
